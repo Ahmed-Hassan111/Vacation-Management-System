50da36783381f0a87cc34263ecb3b5fa47a3feef
Ahmed-Hassan111 <https://github.com/Ahmed-Hassan111>
2024-01-28 16:24:09 +0200

    Vacation Management System App

---
+++ Controllers/DepratmentsController.cs
@@ -0,0 +1,69 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VacationManagement.Data;
+using VacationManagement.Models;
+
+namespace VacationManagement.Controllers
+{
+    public class DepratmentsController : Controller
+    {
+        private readonly VacationDbContext _context;
+        public DepratmentsController(VacationDbContext context)
+        {
+            _context = context;
+        }
+        public IActionResult Depratments()
+        {
+
+            return View(_context.Departments.OrderBy(x=>x.Name).ToList());
+        }
+        public IActionResult Create()
+        {
+            return View();
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Department model)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Departments.Add(model);
+                _context.SaveChanges();
+                return RedirectToAction("Depratments");
+            }
+            return View(model);
+        }
+        public IActionResult Edit(int Id)
+        {  
+            return View(_context.Departments.FirstOrDefault(x=>x.Id == Id));
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Edit(Department model)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Departments.Update(model);
+                _context.SaveChanges();
+                return RedirectToAction("Depratments");
+            }
+            return View(model);
+        }
+        public IActionResult Delete(int Id)
+        {
+            return View(_context.Departments.FirstOrDefault(x => x.Id == Id));
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(Department model)
+        {
+           
+            if(model != null)
+            {
+                _context.Departments.Remove(model);
+                _context.SaveChanges();
+                return RedirectToAction("Depratments");
+            }
+            return View(model);
+        }
+    }
+}
---
+++ Models/VacationType.cs
@@ -0,0 +1,16 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace VacationManagement.Models
+{
+    public class VacationType : EntityBase
+    {
+        [Display(Name = "Vacation Name")]
+        [StringLength(200)]
+        public string VacationName { get; set; }
+        [Display(Name = "Vacation Color")]
+        [StringLength(7)]
+        public string backgroundColor { get; set; }
+        [Display(Name = "Number Days")]
+        public int NumberDays { get; set; }
+    }
+}
